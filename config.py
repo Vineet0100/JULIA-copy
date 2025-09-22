25d037d5981360d28de9d6fb0bf028190f300e24
Vineet0100 <vineetjangir001@gmail.com>
2025-09-22 21:10:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Gemini AI API key
+GEMINI_API_KEY = "AIzaSyC3_9wBA-Ooe260lYNrrnw9MO3faukKZRE"
+
+# Google Search API credentials
+GOOGLE_API_KEY = "AIzaSyD9nc2ohB1JhvR65tkVEzC5G-a_U58G7Yk"
+CSE_ID = "16cd1f3f3fe7a4455"
